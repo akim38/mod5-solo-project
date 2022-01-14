b3a073cd946d77047299609f9b34922341e3f870
aletheia <aletheiak93@gmail.com>
2022-01-14 05:01:08 -0800

    styled splash page

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -4,10 +4,11 @@ import './SplashPage.css'
 
 const SplashPage = () => {
     return (
-        <div>
-            <div>
+        <>
+        <div className='splash-area'>
+            <div className='splash-text'>
                 <h2>Welcome to PokéMeetup!</h2>
-                <p>PokéMeetup is here to connect you to other trainers and Pokéfans!
+                <p>PokéMeetup is here to connect you to trainers, Pokéfans, and all others in the Pokémon world!
                 Connecting everyone across all regions through our shared interest
                 in those companions we call Pokémon.</p>
             </div>
@@ -15,6 +16,11 @@ const SplashPage = () => {
                 Checkout Upcoming Events!
             </NavLink>
         </div>
+        <div className='about'>
+            <p>Developed by Aletheia Kim 2022</p>
+            <a href='https://github.com/akim38'>Github</a>
+        </div>
+        </>
 
     )
 }
